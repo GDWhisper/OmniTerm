f2fe606550053fd10efe145c1f32d5515020df65
vacioo <vacioo@live.com>
2026-06-23 18:24:14 +0800

    revert: remove react-smart-mouse and react-cursor-position

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import { Layout } from './components/Layout/Layout'
 import { ToastContainer } from './components/Toast/Toast'
 import { useMobileDetection } from './hooks/useMediaQuery'
-import ReactCursorPosition from 'react-cursor-position'
 
 function App() {
   useMobileDetection()
 
   return (
     <>
-      <ReactCursorPosition>
-        <Layout />
-      </ReactCursorPosition>
+      <Layout />
       <ToastContainer />
     </>
   )
